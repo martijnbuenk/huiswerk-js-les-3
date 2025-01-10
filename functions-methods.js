4fb1b589f33b4ba77f8fdbcf0db089cb278c0848
martijn buenk <martijnbuenk@hotmail.com>
2025-01-10 16:24:14 +0100

    huiswerk-vrijdag-einde

--- functions-methods.js
+++ functions-methods.js
@@ -1,7 +1,6 @@
 // Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
 // Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
 
-
 /* Opdracht  1 */
 // Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft. Een domeinnaam is hetgeen dat na het @ in het adres staat
 // ---- Verwachte uitkomsten:
@@ -9,17 +8,14 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
-let email = "";
 function getEmailDomain(email) {
 const domainName = email.split("@");
   return domainName[1];
 }
 
-console.log(getEmailDomain(email + "n.eeken@novi-education.nl"));
-console.log(getEmailDomain(email + "t.mellink@novi.nl"));
-console.log(getEmailDomain(email + "a.wiersma@outlook.com"));
-
-
+console.log(getEmailDomain("n.eeken@novi-education.nl"));
+console.log(getEmailDomain("t.mellink@novi.nl"));
+console.log(getEmailDomain("a.wiersma@outlook.com"));
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -29,37 +25,26 @@ console.log(getEmailDomain(email + "a.wiersma@outlook.com"));
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
-//input
-//input emailadres
-
-//output
-//domeinnaam
-
-//stappenplan
-//stap 1 een variable maken om het email adres in op te slaan
-//stap 2 functie maken die het type email controleert
-//stap 3 variabele maken om het type email op te slaan
-//stap 4 returnen
-//stap 5 if statement maken op de return  met conditie === gelijk aan het genoemde domein met de gewenste tekst
-//stap 6 else if statement maken als bovengenoemde conditie false is moet dan gelijk zijn aan het genoemde domein en dan de gewenste tekst
-//stap 7 statement loggen
-
-
-let emailTwo = "";
 function typeOfEmail(email) {
-  const domainNameTwo = emailTwo.split("@");
-  //if (domainNameTwo[1] === "novi-education.nl") {
-    //'Student'
-  //} else if (domainNameTwo[1] === "novi.nl") {
-    //'Medewerker'
-  //}
+  const emailType = email.split("@");
+  if (emailType[1] === "novi-education.nl") {
+    emailType[1] = "Student"
+    return emailType[1];
 
-console.log(domainNameTwo[1]);
+  } else if (emailType[1] === "novi.nl") {
+    emailType[1] = "Medewerker"
+    return emailType[1];
 
-
-
-
-
+  } else (emailType[1] = "outlook.com")
+  {
+    emailType[1] = "Extern"
+    return emailType[1];
+  }
+}
+console.log(typeOfEmail("n.eeken@novi-education.nl"))
+console.log(typeOfEmail("t.mellink@novi.nl"))
+console.log(typeOfEmail("novi.nlaapjesk@outlook.com"))
+console.log(typeOfEmail("a.wiersma@outlook.com"))
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
 // Een emailadres is valide wanneer:
@@ -72,3 +57,18 @@ console.log(domainNameTwo[1]);
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(email) {
+  const emailValidity = email.slice(-1);
+  if (emailValidity === ".") {
+    return false
+  }
+ return emailValidity;
+  else if (emailValidity === "@") {
+    return true
+  }
+  return emailValidity
+
+}
+
+console.log(checkEmailValidity("n.eeken@novi.nl"))
